486dd1805248e3f9b0b03d93a53fbaf2b3ec7218
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-18 19:52:21 +0200

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { authReducer } from "./auth/slice";
-import { contactsReducer } from "./contacts/slice";
-import { filtersReducer } from "./filters/slice";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import authReducer from './auth/slice'; 
+import contactsReducer from './contacts/slice';
+import filterReducer from './filters/slice';
 
 const authPersistConfig = {
-  key: "auth",
+  key: 'auth',
   storage,
-  whitelist: ["token"],
+  whitelist: ['token'],
 };
 
-const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
-
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    auth: persistedAuthReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
-    filters: filtersReducer,
+    filter: filterReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
@@ -37,3 +26,4 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
+export default store;
