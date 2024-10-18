21f4fe8f4130bd809070d306b587bad103428e84
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-18 19:24:48 +0200

    1

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn, selectIsRefreshing } from '../../redux/auth/selectors';
+
+
+const PrivateRoute = ({ component: Component, redirectTo = '/login' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
+
+  return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+export default PrivateRoute;
---
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -0,0 +1,12 @@
+
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+
+const RestrictedRoute = ({ component: Component, redirectTo = '/contacts' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+export default RestrictedRoute;
