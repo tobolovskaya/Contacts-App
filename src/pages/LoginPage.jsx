a79fde948e05201a58a6b93738b72c172fc9ff9e
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-10-22 11:03:42 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,32 +1,35 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { login } from '../redux/auth/operations';
-import { selectIsLoggedIn } from '../redux/auth/selectors';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { Formik, Form, Field } from "formik";
+import { logIn } from "../redux/auth/operations";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const navigate = useNavigate();
 
   const handleLogin = (credentials) => {
-    dispatch(login(credentials));
+    dispatch(logIn(credentials));
   };
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/contacts'); 
-    }
-  }, [isLoggedIn, navigate]);
 
   return (
     <div>
       <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleLogin}
+      >
+        <Form>
+          <div>
+            <label>Email</label>
+            <Field name="email" type="email" />
+          </div>
+          <div>
+            <label>Password</label>
+            <Field name="password" type="password" />
+          </div>
+          <button type="submit">Login</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
 
 export default LoginPage;
-
